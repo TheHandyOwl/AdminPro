45079fe7b41adb8f8a4a2ba578b42fb16a003066
Alan Casas <alancasasarevalo@gmail.com>
2018-03-04 19:18:30 +0100

    Register component added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { PagesComponent } from './pages/pages.component';
 
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LoginComponent } from './login/login.component';
 import { ProgressComponent } from './pages/progress/progress.component';
 import { Graphics1Component } from './pages/graphics1/graphics1.component';
 import { PagedidnotfoundComponent } from './shared/pagedidnotfound/pagedidnotfound.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './login/register.component';
 
 const appRoutes: Routes = [
     { path : '', component : PagesComponent,
@@ -17,7 +18,7 @@ const appRoutes: Routes = [
         { path : '', redirectTo: 'dashboard', pathMatch: 'full'},
     ]},
     { path : 'login', component : LoginComponent},
-    { path : 'register', component : DashboardComponent},
+    { path : 'register', component : RegisterComponent},
     { path : '**', component: PagedidnotfoundComponent}
 ];
 
