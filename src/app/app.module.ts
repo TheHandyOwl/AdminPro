d50c9a79b51bc7b745de9e490f6d263c4baf22d0
Alan Casas <alancasasarevalo@gmail.com>
2018-03-04 16:14:36 +0100

    Login, dashboard, graphics1, progress, breadcrumbs, header pagedidnotfound and sidebar components were added shared, sidebar services were added css templates were added js templates were added plugin templates were added SASS templates were added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,27 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { PagedidnotfoundComponent } from './shared/pagedidnotfound/pagedidnotfound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graphics1Component } from './pages/graphics1/graphics1.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PagedidnotfoundComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Graphics1Component,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/shared/pagedidnotfound/pagedidnotfound.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pagedidnotfound',
+  templateUrl: './pagedidnotfound.component.html',
+  styles: []
+})
+export class PagedidnotfoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
